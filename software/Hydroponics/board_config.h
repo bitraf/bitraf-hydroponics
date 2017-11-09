731a1434bbf7e6a06b5772d7b59626b7877d6abb
Daniel Flehner Heen <flehnerheener@gmail.com>
2017-11-09 19:12:04 +0100

    added support for both NodeMCU and ESP-12E

--- software/Hydroponics/board_config.h
+++ software/Hydroponics/board_config.h
@@ -6,26 +6,53 @@
 
 #define FORMAT_SPIFFS false
 
-// GPIO pins
 
-// RF transmit
-#define RF_DATA D4
+// NodeMCU
+#if defined ARDUINO_ESP8266_NODEMCU
+  // RF transmit
+  #define RF_DATA D4
 
-// Switches
-#define SWITCH_1 D0
-#define SWITCH_2 D5
-#define SWITCH_3 D6
+  // Switches
+  #define SWITCH_1 D0
+  #define SWITCH_2 D5
+  #define SWITCH_3 D6
 
-// Rotary encoder
-#define ROT_0 D3
-#define ROT_1 D2
-#define ROT_SW D1
+  // Rotary encoder
+  #define ROT_0 D3
+  #define ROT_1 D2
+  #define ROT_SW D1
 
-// Buzzer
-#define BUZZ D7
+  // Buzzer
+  #define BUZZ D7
 
-// Temp hum
-#define TEMP_HUM D8
+  // Temp hum
+  #define TEMP_HUM D8
 
-// LDR
-#define LDR A0
+  // LDR
+  #define LDR A0
+
+// ESP8266 Chip only
+#elif defined ARDUINO_ESP8266_ESP01
+  // RF transmit
+  #define RF_DATA 11
+
+  // Switches
+  #define SWITCH_1 4
+  #define SWITCH_2 5
+  #define SWITCH_3 6
+
+  // Rotary encoder
+  #define ROT_0 12
+  #define ROT_1 13
+  #define ROT_SW 14
+
+  // Buzzer
+  #define BUZZ 7
+
+  // Temp hum
+  #define TEMP_HUM 10
+
+  // LDR
+  #define LDR 2
+
+#endif
